4ba69a05c5a26e5895fb21a369f6a50213816d5c
Joseph Chang <josephdanielchang@yahoo.com>
2019-11-06 16:41:40 -0800

    Running properly at 30 fps

--- rs-multicam.cpp
+++ rs-multicam.cpp
@@ -1,6 +1,13 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
 
+/*
+	Made for streaming with two Intel RealSense cameras
+	Frames saved in fssLeft and fssRight
+	Commented out if(POINTCLOUD) and if(SAVE_PNG) to prevent fps lowering
+	Before running: enable SAVE_PNG to check frames saved in correct directories - if not invert CAM_SWITCHED
+*/
+
 #include <librealsense2/rs.hpp>     // Include RealSense Cross Platform API
 #include <opencv2/opencv.hpp>
 #include "example.hpp"              // Include short list of convenience functions for OpenGL rendering
@@ -19,8 +26,31 @@
 #define WIDTH 640						// SET stream width
 #define HEIGHT 480						// SET stream height
 #define FPS 30							// SET camera fps (6,15,30,60)
-#define TIMESTAMP true					// SET false to disable saving timestamps in .txt, time in seconds
+#define TIMESTAMP true					// ENABLE saving timestamps in .txt, seconds since epoch
+#define SAVE_PNG false					// ENABLE saving rgb and depth in .png, lowers fps
+#define POINTCLOUD false				// ENABLE saving pointcloud in .ply
 #define CAM_SWITCHED true				// INVERT if left camera saving right images vice-versa
+
+using namespace cv;
+
+char buffer[50];
+Mat image1_bgr;
+
+void writeImagesPNG(rs2::frameset const& f, std::string& dir, int count, std::string cam) {
+	std::stringstream path1, path2;
+	// Set up path for images
+	sprintf(buffer, "%05d", count);
+	path1 << dir << "\\" << cam << "_rgb\\cam_" << cam << "_rgb_" << buffer << ".png";
+	path2 << dir << "\\" << cam << "_intel_depth\\cam_" << cam << "_depth_" << buffer << ".png";
+	// Create OpenCV image file, 8-bit, unsigned, 3 channels
+	Mat image1(Size(WIDTH, HEIGHT), CV_8UC3, (void*)f.get_color_frame().get_data(), Mat::AUTO_STEP);
+	// Transform color format
+	cvtColor(image1, image1_bgr, COLOR_RGB2BGR);
+	imwrite(path1.str(), image1_bgr);
+	// Create OpenCV image file, 16-bit, unsigned, 1 channel
+	Mat image2(Size(WIDTH, HEIGHT), CV_16UC1, (void*)f.get_depth_frame().get_data(), Mat::AUTO_STEP);
+	imwrite(path2.str(), image2);
+}
 
 void save_timestamp(std::string& dir, int count, std::string cam, double rgb_t, double depth_t) {
 	// Set up path for timestamps
@@ -36,6 +66,20 @@ void save_timestamp(std::string& dir, int count, std::string cam, double rgb_t, 
 	outfile2.close();
 }
 
+// Declare pointcloud object, for calculating pointclouds and texture mappings
+rs2::pointcloud pc;
+// We want the points object to be persistent so we can display the last cloud when a frame drops
+rs2::points points;
+
+void writePointCloud(rs2::frameset const& f, std::string& dir, int count, std::string cam) {
+	// Set up path for pointclouds
+	sprintf(buffer, "%05d", count);
+	std::string path(dir + "\\" + cam + "_pointcloud\\cam_" + cam + "_pointcloud_" + buffer + ".ply");
+	pc.map_to(f.get_color_frame());
+	points = pc.calculate(f.get_depth_frame());
+	points.export_to_ply(path, f.get_color_frame());
+}
+
 int main(int argc, char* argv[]) try
 {
 	// Create a simple OpenGL window for rendering:
@@ -44,7 +88,6 @@ int main(int argc, char* argv[]) try
 	rs2::context                          ctx;        // Create librealsense context for managing devices
 	std::map<std::string, rs2::colorizer> colorizers; // Declare map from device serial number to colorizer (utility class to convert depth data RGB colorspace)
 	std::vector<rs2::pipeline>            pipelines;
-
 
 	// We'll keep track of the last frame of each stream available to make the presentation persistent
 	std::map<int, rs2::frame> render_frames;
@@ -55,11 +98,7 @@ int main(int argc, char* argv[]) try
 	std::string left = "left";
 	std::string right = "right";
 
-	int frameNum = 0;
-	double rgb_t;				//timestamp
-	double depth_t;				//timestamp
 	bool isLeft = true;
-
 
 	// Create directories to store processed frames
 	for (auto dir : dirs) {									// for 2 directories
@@ -90,8 +129,7 @@ int main(int argc, char* argv[]) try
 		rs2::sensor depth = dev.query_sensors()[0];
 		rs2::sensor color = dev.query_sensors()[1];
 		depth.set_option(RS2_OPTION_INTER_CAM_SYNC_MODE, mode);
-		mode = 2;
-		
+
 		// Allow sensor to dynamically adjust fps depending on lighting conditions
 		//color.set_option(RS2_OPTION_AUTO_EXPOSURE_PRIORITY, 0.f);
 
@@ -105,6 +143,11 @@ int main(int argc, char* argv[]) try
 		// Start streaming
 		pipe.start(cfg);
 		pipelines.emplace_back(pipe);
+		//if (mode == 1)
+			//pipelines.emplace_back(pipe);
+
+		mode = 2;
+
 		// Map from each device's serial number to a different colorizer
 		colorizers[dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER)] = rs2::colorizer();
 	}
@@ -112,29 +155,56 @@ int main(int argc, char* argv[]) try
 	// Define object to be used to align to depth to color stream
 	rs2::align align_to_color(RS2_STREAM_COLOR);
 
+	// Capture frames to give autoexposure, etc. a chance to settle
+	//for (auto i = 0; i < 30; ++i)
+	//	for (auto&& pipe : pipelines) pipe.wait_for_frames();
+
+	int frameNum = -200;
+	double rgb_t;				//timestamp
+	double depth_t;				//timestamp
+	bool discardDone = false;
+	rs2::frameset fs;
+	std::vector <rs2::frameset> fssLeft;	// holds all left camera frames
+	std::vector <rs2::frameset> fssRight;	// holds all right camera frames
+
 	// Main app loop
 	while (app)
 	{
 		// Collect the new frames from all the connected devices
 		std::vector<rs2::frame> new_frames;
+
 		for (auto&& pipe : pipelines)
 		{
-			rs2::frameset fs;
 			if (pipe.poll_for_frames(&fs))
 			{
 				// Align newly-arrived frames to color viewport
 				fs = align_to_color.process(fs);
-
 				// Save frame timestamps to directories
 				if (TIMESTAMP) {
 					rgb_t = fs.get_color_frame().get_timestamp();
 					depth_t = fs.get_depth_frame().get_timestamp();
-					if (isLeft)
-						save_timestamp(dirs[0], frameNum, left, rgb_t, depth_t);
-					else
-						save_timestamp(dirs[1], frameNum, right, rgb_t, depth_t);
+					if (frameNum >= 0) {
+						if (isLeft) {
+							save_timestamp(dirs[0], frameNum, left, rgb_t, depth_t);
+							fssLeft.emplace_back(std::move(fs.get_depth_frame()));
+							//std::cout << "fssLeft size: " << fssLeft.size() << '\n'; // how many left frames passed by
+							//if (POINTCLOUD)
+							//	writePointCloud(fs, dirs[0], frameNum, left);
+							//if (SAVE_PNG)
+							//	writeImagesPNG(fs, dirs[0], frameNum, left);
+						}
+						else {
+							save_timestamp(dirs[1], frameNum, right, rgb_t, depth_t);
+							fssRight.emplace_back(std::move(fs.get_color_frame()));
+							//std::cout << "fssRight size: " << fssRight.size() << '\n'; // how many right frames passed by
+							//if (POINTCLOUD)
+							//	writePointCloud(fs, dirs[1], frameNum, right);
+							//if (SAVE_PNG)
+							//	writeImagesPNG(fs, dirs[1], frameNum, right);
+						}
+					}
 				}
-				
+
 				isLeft = !isLeft;
 
 				for (const rs2::frame& f : fs)
